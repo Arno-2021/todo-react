2bf6c59c8172f7f009874d652947ccf31a0da823
Arno-2021 <aa307204890@gmail.com>
2021-11-02 17:18:10 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+import '../node_modules/todomvc-common/base.css'
+import '../node_modules/todomvc-app-css/index.css'
+ReactDOM.render(<App />, document.querySelector('#root'))
